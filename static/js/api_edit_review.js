b62f7f695cf15dcef1203cc225ab594faa80bdc1
mlgranger540 <mattgranger540@outlook.com>
2023-05-22 17:05:30 +0100

    Changes to edit review page and logic

--- static/js/api_edit_review.js
+++ static/js/api_edit_review.js
@@ -1,18 +1,23 @@
 document.addEventListener("DOMContentLoaded", async function(event) {
-    await fetch('/api/reviews',{method:"GET"}).then(response => response.json()).then((data)=>{
-        console.log(data);
-        let reviewDropdown = document.getElementById("review_dropdown");
-        for (let i in data) {
-            let reviewTitle = data[i].ReviewTitle;
-            let artistName = data[i].ArtistName;
-            let venueName = data[i].VenueName;
-            let concertDate = data[i].ConcertDate;
-            let concertID = data[i].concert_id;
-            let reviewName = reviewTitle + " - " + artistName + " @ " + venueName + " " + concertDate;
-            var option = document.createElement("option");
-            option.text = reviewName;
-            option.value = concertID;
-            reviewDropdown.appendChild(option);
-        }
-    })
+    const reviews_res = await fetch('/api/reviews',{method:"GET"});
+    const reviews_json = await reviews_res.json();
+    const reviews = reviews_json;
+    var reviewDropdown = document.getElementById("review_dropdown");
+    for (let i in reviews) {
+        let reviewTitle = reviews[i].ReviewTitle;
+        let artistName = reviews[i].ArtistName;
+        let venueName = reviews[i].VenueName;
+        let reviewID = reviews[i].review_id;
+        let concertID = reviews[i].concert_id;
+        let reviewName = reviewTitle + " - " + artistName + " @ " + venueName;
+        var option = document.createElement("option");
+        option.text = reviewName;
+        option.value = reviewID;
+        option.id = concertID;
+        reviewDropdown.appendChild(option);
+    }
 });
+
+reviewDropdown.addEventListener("change", async function(event) {
+    console.log(event);
+})
